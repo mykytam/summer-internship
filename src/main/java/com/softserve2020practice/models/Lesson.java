254b623eb167caa5fc574a42a6a7db6281ee847a
Leva <kiriienko.l.s@nmu.one>
2020-06-26 11:58:15 +0300

    Create Lesson Entity

--- src/main/java/com/softserve2020practice/models/Lesson.java
+++ src/main/java/com/softserve2020practice/models/Lesson.java
@@ -1,4 +1,15 @@
 package com.softserve2020practice.models;
 
+import java.sql.Timestamp;
+
 public class Lesson {
+    private long id;
+
+    private long mentorId;
+
+    private long groupId;
+
+    private long themeId;
+
+    private Timestamp lessonDate;
 }
