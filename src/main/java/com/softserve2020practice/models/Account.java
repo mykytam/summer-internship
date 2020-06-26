e23d6e47b871983509976591884a5c9de20d8f5e
MentTea <mentteabags@gmail.com>
2020-06-26 15:39:08 +0300

    Change space in the Models

--- src/main/java/com/softserve2020practice/models/Account.java
+++ src/main/java/com/softserve2020practice/models/Account.java
@@ -3,11 +3,17 @@ package com.softserve2020practice.models;
 public class Account {
 
     private long id;
+
     private byte role;
+
     private String firstName;
+
     private String lastName;
+
     private String email;
+
     private String password;
+
     private String salt;
 
 }
--- src/main/java/com/softserve2020practice/models/Visit.java
+++ src/main/java/com/softserve2020practice/models/Visit.java
@@ -3,9 +3,13 @@ package com.softserve2020practice.models;
 public class Visit {
 
     private long idTrainee;
+
     private long idLesson;
+
     private byte studentMark;
+
     private boolean presence;
+
     private String comment;
 
 }
