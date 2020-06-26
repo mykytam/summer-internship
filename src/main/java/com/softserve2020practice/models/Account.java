dfad759d87d6976a960b3cfa110f3cd7d9ebae82
mykytam <morar.nikita@gmail.com>
2020-06-26 11:13:15 +0300

    Create fields in Account model

--- src/main/java/com/softserve2020practice/models/Account.java
+++ src/main/java/com/softserve2020practice/models/Account.java
@@ -1,4 +1,15 @@
 package com.softserve2020practice.models;
 
+import com.softserve2020practice.models.enums.Role;
+
 public class Account {
+
+    private Long id;
+    private Role role;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String salt;
+
 }
