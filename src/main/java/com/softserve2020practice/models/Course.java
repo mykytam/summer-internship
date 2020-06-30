6a440ba1417b93359206ed8ff09498d9c6dcf8dd
MentTea <mentteabags@gmail.com>
2020-06-30 15:12:50 +0300

    Add annotations to Course

--- src/main/java/com/softserve2020practice/models/Course.java
+++ src/main/java/com/softserve2020practice/models/Course.java
@@ -1,9 +1,23 @@
 package com.softserve2020practice.models;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "Course")
 public class Course {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Long id;
 
+    @Column(name = "name")
     private String name;
 
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
 }
