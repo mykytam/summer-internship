b8b4c9a13d6b6297aa2481da6b32d6ae58f085a8
mykytam <morar.nikita@gmail.com>
2020-06-30 20:19:38 +0300

    Link StudentGroup and Account

--- src/main/java/com/softserve2020practice/models/MentorsOfStudentGroup.java
+++ src/main/java/com/softserve2020practice/models/MentorsOfStudentGroup.java
@@ -1,13 +1,29 @@
 package com.softserve2020practice.models;
 
-public class MentorsOfStudentGroup {
+import lombok.Data;
 
-    private long id;
+import javax.persistence.*;
+import java.io.Serializable;
 
-    private long mentorId;
+@Entity
+@Data
+@Table(name = "MentorsOfStudentGroups")
+public class MentorsOfStudentGroup implements Serializable {
 
-    private long studentGroupId;
+    @EmbeddedId
+    private MentorsOfStudentGroupPK id;
 
+    @ManyToOne
+    @MapsId("mentorId")
+    @JoinColumn(name = "IdMentor")
+    private Account mentor;
+
+    @ManyToOne
+    @MapsId("studentGroupId")
+    @JoinColumn(name = "IdStudentGroup")
+    private StudentGroup studentGroup;
+
+    @Column(name = "Comments")
     private String comment;
 
 }
