e168c97866240c70b0d308938849b025326f766f
MentTea <mentteabags@gmail.com>
2020-06-30 14:55:45 +0300

    Add annotations to Account

--- src/main/java/com/softserve2020practice/models/Account.java
+++ src/main/java/com/softserve2020practice/models/Account.java
@@ -1,7 +1,17 @@
 package com.softserve2020practice.models;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
 public class Account {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
     private byte role;
@@ -17,5 +27,8 @@ public class Account {
     private String salt;
 
     private boolean active;
+
+    @ManyToMany(fetch = FetchType.EAGER, mappedBy = "StudentsOfGroups")
+    private Set<StudentGroup> studentGroupSet = new HashSet<>();
 
 }
