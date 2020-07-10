c1f4fbe6057cedd13474c0d722c904ba3f1d45bf
mykytam <morar.nikita@gmail.com>
2020-07-10 17:19:01 +0300

    Add Swagger config and email validation

--- src/main/java/com/softserve2020practice/SoftServe2020PracticeApplication.java
+++ src/main/java/com/softserve2020practice/SoftServe2020PracticeApplication.java
@@ -2,45 +2,11 @@ package com.softserve2020practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Collections;
 
 @SpringBootApplication
-@EnableSwagger2
 public class SoftServe2020PracticeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SoftServe2020PracticeApplication.class, args);
 	}
-
-	@Bean
-	public Docket swaggerConfig() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.paths(PathSelectors.ant("/api/*"))
-				.apis(RequestHandlerSelectors.basePackage("com.softserve2020practice"))
-				.build()
-				.apiInfo(apiDetails());
-	}
-
-	private ApiInfo apiDetails() {
-		return new ApiInfo(
-				"SoftServe Summer Project 2020 API made by Romeo Team",
-				"REST API for IT courses",
-				"1.0",
-				"Free to use",
-				new springfox.documentation.service.Contact("Romeo Team", "", ""),
-				"API License",
-				"",
-				Collections.emptyList());
-	}
-
-
 }
--- src/main/java/com/softserve2020practice/dto/MentorCreateDto.java
+++ src/main/java/com/softserve2020practice/dto/MentorCreateDto.java
@@ -4,6 +4,7 @@ import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import java.util.Set;
 
 @Data
@@ -11,6 +12,10 @@ import java.util.Set;
 public class MentorCreateDto {
 
     @NotBlank
+    @Pattern(
+            regexp = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$",
+            message = "Invalid email"
+    )
     private String email;
 
     @NotBlank
