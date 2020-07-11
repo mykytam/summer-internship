80ddfae94438f3f98b495af08dffc7c9f7175de2
HeyMan <lev.kirienko.00@gmail.com>
2020-07-11 19:11:15 +0000

    Resolve "Implement exception handling"

--- src/main/java/com/softserve2020practice/exceptions/StudentNotFoundException.java
+++ src/main/java/com/softserve2020practice/exceptions/StudentNotFoundException.java
@@ -1,7 +1,5 @@
 package com.softserve2020practice.exceptions;
 
-import javax.persistence.EntityNotFoundException;
-
 public class StudentNotFoundException extends EntityNotFoundException {
 
     public StudentNotFoundException(String message) {
