62e78d1314d8117ecc4996e0211e74314a91386e
HeyMan <lev.kirienko.00@gmail.com>
2020-07-11 20:13:25 +0300

    Fix EntityNotFoundException

--- src/main/java/com/softserve2020practice/exceptions/CourseNotFoundException.java
+++ src/main/java/com/softserve2020practice/exceptions/CourseNotFoundException.java
@@ -1,7 +1,5 @@
 package com.softserve2020practice.exceptions;
 
-import javax.persistence.EntityNotFoundException;
-
 public class CourseNotFoundException extends EntityNotFoundException {
 
     public CourseNotFoundException(String message) {
