fa624f9eae52b18612733b1052f557e0b19a7f3d
mykytam <morar.nikita@gmail.com>
2020-07-12 17:13:50 +0300

    Add accesses to controller

--- src/main/java/com/softserve2020practice/controllers/LessonController.java
+++ src/main/java/com/softserve2020practice/controllers/LessonController.java
@@ -4,6 +4,7 @@ import com.softserve2020practice.dto.LessonByStudentIdResponseDto;
 import com.softserve2020practice.dto.LessonCreateDto;
 import com.softserve2020practice.dto.LessonResponseDto;
 import com.softserve2020practice.dto.LessonUpdateDto;
+import com.softserve2020practice.security.Access;
 import com.softserve2020practice.services.LessonService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,30 +21,35 @@ public class LessonController {
     private final LessonService lessonService;
 
     @GetMapping
+    @Access.Mentor
     @ResponseStatus(HttpStatus.OK)
     public List<LessonResponseDto> getAllLessons() {
         return lessonService.getAllLessons();
     }
 
     @GetMapping(value = "/students/{id}")
+    @Access.MentorAndStudent
     @ResponseStatus(HttpStatus.OK)
     public List<LessonByStudentIdResponseDto> getLessonsByStudentId(@PathVariable Long id) {
         return lessonService.getLessonsByStudentId(id);
     }
 
     @PostMapping
+    @Access.Mentor
     @ResponseStatus(HttpStatus.CREATED)
     public void createLesson(@Valid @RequestBody LessonCreateDto lessonCreateDto) {
         lessonService.addLesson(lessonCreateDto);
     }
 
     @PutMapping(value = "{id}")
+    @Access.Mentor
     @ResponseStatus(HttpStatus.OK)
     public void updateLesson(@Valid @RequestBody LessonUpdateDto lessonUpdateDto, @PathVariable Long id) {
         lessonService.updateLesson(id, lessonUpdateDto);
     }
 
     @DeleteMapping(value = "{id}")
+    @Access.Mentor
     @ResponseStatus(HttpStatus.OK)
     public void deleteLesson(@PathVariable Long id) {
         lessonService.deleteLesson(id);
