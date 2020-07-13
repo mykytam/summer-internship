b45f37cb648134e6ad02ac3b0fdf9ae11065b12c
MentTea <mentteabags@gmail.com>
2020-07-13 17:35:04 +0300

    Add ExceptionHandlingFunctionality in LessonImpl

---
+++ src/main/java/com/softserve2020practice/exceptions/VisitNotFoundException.java
@@ -0,0 +1,9 @@
+package com.softserve2020practice.exceptions;
+
+public class VisitNotFoundException extends EntityNotFoundException {
+
+    public VisitNotFoundException(String message) {
+        super(message);
+    }
+
+}
